f8498d19382d94cc1cf6b3f3bdae0e89ce31e92f
Stanislav Evseev <stanislav.evseev@kpn.com>
2018-09-14 11:24:53 +0200

    Added upload/download file test

--- Dockerfiles/Test/tests/test_upload_file/test_2.py
+++ Dockerfiles/Test/tests/test_upload_file/test_2.py
@@ -4,21 +4,39 @@ import requests
 
 import os
 
-# os.path.dirname(__file__)
 file_path = os.path.join('volume' 'file.txt')
-# print(file_path)
+
+
+def get_hash(path):
+    import hashlib as hash
+
+    # Specify how many bytes of the file you want to open at a time
+    BLOCKSIZE = 65536
+
+    sha = hash.sha256()
+    with open(path, 'rb') as kali_file:
+        file_buffer = kali_file.read(BLOCKSIZE)
+        while len(file_buffer) > 0:
+            sha.update(file_buffer)
+            file_buffer = kali_file.read(BLOCKSIZE)
+
+    return sha.hexdigest()
 
 
 def test_customer_1_upload_file():
-    # response = requests.post('http://customer_1:8180/share/create/v1')
+    shared_volume = '/volume'
+    origin_filename = 'file.txt'
 
-    # assert response.status_code == 200
-    # assert response.json()['status'] == 'OK', response.json()
+    directory_local_file = '%s/%s' % (shared_volume, origin_filename)
 
-    # key_id = response.json()['result'][0]['key_id']
-    key_id = 'master$customer1@is_8084'
+    key_id = 'master$customer_1@is_8084'
+    virtual_file = 'virtual_file.txt'
 
-    remote_path = '%s:%s' % (key_id, 'virtual_file.txt')
+    remote_path = '%s:%s' % (key_id, virtual_file)
+
+    download_volume = '/download_volume'
+
+    directory_dowloaded_file = '%s/%s' % (download_volume, virtual_file)
 
     response = requests.post('http://customer_1:8180/file/create/v1', json={'remote_path': remote_path})
     assert response.status_code == 200
@@ -26,7 +44,7 @@ def test_customer_1_upload_file():
 
     response = requests.post(
         'http://customer_1:8180/file/upload/start/v1',
-        json={'remote_path': remote_path, 'local_path': '/volume/file.txt', 'wait_result': 'true'}
+        json={'remote_path': remote_path, 'local_path': directory_local_file, 'wait_result': 'true'}
     )
     assert response.status_code == 200
     assert response.json()['status'] == 'OK', response.json()
@@ -34,7 +52,7 @@ def test_customer_1_upload_file():
     for i in range(100):
         response = requests.post(
             'http://customer_1:8180/file/download/start/v1',
-            json={'remote_path': remote_path, 'destination_path': '/download_volume', 'wait_result': 'true'}
+            json={'remote_path': remote_path, 'destination_folder': download_volume, 'wait_result': 'true'}
         )
         assert response.status_code == 200
 
@@ -48,7 +66,9 @@ def test_customer_1_upload_file():
             print(response.json())
             assert False, response.json()
     else:
+        print('download was not successful')
         assert False, response.json()
 
-        # assert response.json()['status'] == 'OK', response.json()
-    print(response.json())
+    assert os.path.exists(directory_dowloaded_file)
+
+    assert get_hash(directory_local_file) == get_hash(directory_dowloaded_file)
